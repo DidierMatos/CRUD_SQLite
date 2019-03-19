3c2f744d853fd6bec2bac329dc0d4e0c23aa9ffc
Didier Matos <d.matostorres@gmail.com>
2019-03-19 09:53:01 -0500

    Solicitar permiso y funcionamiento al presionar el ImageView y Respuesta integrada al rechazar el permiso

--- app/src/main/java/com/ndevelopers/crud_sqlite/MainActivity.java
+++ app/src/main/java/com/ndevelopers/crud_sqlite/MainActivity.java
@@ -1,17 +1,25 @@
 package com.ndevelopers.crud_sqlite;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText mEdtName, mEdtAge, mEdtPhone;
     Button mBtnAdd, mBtnList;
     ImageView mImageView;
+
+    final int REQUEST_CODE_GALLERY = 999;
 
 
     @Override
@@ -32,6 +40,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // read external storage permission to select image form gallery
+                //runtime permission for devices android
+
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY
+                );
             }
         });
 
@@ -54,5 +69,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == REQUEST_CODE_GALLERY){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //gallery intent
+
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, REQUEST_CODE_GALLERY);
+            }
+            else {
+                Toast.makeText(this, "Don't have permission to access file location", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
+
