19e6916172eeadfdae2d7083978a5677f7e028bf
Didier Matos <d.matostorres@gmail.com>
2019-03-19 09:39:50 -0500

    Vinculando Componentes con sus respectivos ID

--- app/src/main/java/com/ndevelopers/crud_sqlite/MainActivity.java
+++ app/src/main/java/com/ndevelopers/crud_sqlite/MainActivity.java
@@ -2,12 +2,57 @@ package com.ndevelopers.crud_sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEdtName, mEdtAge, mEdtPhone;
+    Button mBtnAdd, mBtnList;
+    ImageView mImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEdtName = findViewById(R.id.edtName);
+        mEdtAge = findViewById(R.id.edtAge);
+        mEdtPhone = findViewById(R.id.edtPhone);
+        mBtnAdd = findViewById(R.id.btnAdd);
+        mBtnList = findViewById(R.id.btnList);
+        mImageView = findViewById(R.id.imageView);
+
+        //select image by on image click
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // read external storage permission to select image form gallery
+            }
+        });
+
+        //add record to sqlite
+
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //code later
+            }
+        });
+
+        //show record list
+
+        mBtnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //code later
+
+            }
+        });
+
     }
 }
